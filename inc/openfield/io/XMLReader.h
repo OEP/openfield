f50092636d258657e73b8f9468ca13c3a13fe8cf
Paul Kilgo <paulkilgo@gmail.com>
2013-11-19 00:23:10 -0500

    finish implementing XMLReader

--- inc/openfield/io/XMLReader.h
+++ inc/openfield/io/XMLReader.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <map>
+#include <list>
 #include <expat.h>
 #include "openfield/io/Reader.h"
 
@@ -15,16 +16,20 @@ public:
   virtual void read(std::istream&, Node&) const;
 private:
   typedef std::map<std::string, std::string> AttributeMap;
+  typedef std::list<Node*> NodeList;
 
   XMLReader(const XMLReader&);
   XMLReader& operator=(const XMLReader&);
-  XML_Parser mParser;
 
   void startElement(const std::string&, const AttributeMap&) const;
   void endElement(const std::string&) const;
 
   static void startElementHandler(void*, const XML_Char*, const XML_Char**);
   static void endElementHandler(void*, const XML_Char*);
+
+  XML_Parser mParser;
+  mutable NodeList mNodeStack;
+  mutable unsigned mDepth;
 };
 
 } // namespace io
--- src/io/XMLReader.cpp
+++ src/io/XMLReader.cpp
@@ -1,25 +1,29 @@
+#include <cassert>
 #include "openfield/io/XMLReader.h"
 #include "openfield/Exceptions.h"
+#include "openfield/io/Node.h"
 
 using openfield::io::XMLReader;
 using openfield::io::Node;
 
 XMLReader::XMLReader():
-  mParser(XML_ParserCreate(NULL))
+  mParser(XML_ParserCreate(NULL)), mNodeStack(), mDepth(0)
 {
   XML_SetElementHandler(mParser,
                         XMLReader::startElementHandler,
                         XMLReader::endElementHandler);
+  XML_SetUserData(mParser, this);
 }
 
 XMLReader::~XMLReader() {
   XML_ParserFree(mParser);
 }
 
-void XMLReader::read(std::istream& is, Node&) const {
+void XMLReader::read(std::istream& is, Node& root) const {
   std::string buffer;
   std::getline(is, buffer);
   int isFinal;
+  mNodeStack.push_back(&root);
   do {
     isFinal = is.eof();
     if(0 == XML_Parse(mParser, buffer.c_str(), buffer.size(), isFinal)) {
@@ -29,12 +33,31 @@ void XMLReader::read(std::istream& is, Node&) const {
     }
     std::getline(is, buffer);
   } while(!is.eof());
+  assert(mDepth == 0);
+  assert(mNodeStack.size() == 0);
 }
 
-void XMLReader::startElement(const std::string& name, const AttributeMap&) const {
+void XMLReader::startElement(const std::string& name, const AttributeMap& attrs) const {
+  Node *n = NULL;
+  if(mDepth == 0) {
+    // special case: we're defining the root
+    n = mNodeStack.back();
+    n->setName(name);
+  }
+  else {
+    n = mNodeStack.back()->addChild(name);
+    mNodeStack.push_back(n);
+  }
+  ++mDepth;
+
+  for(const AttributeMap::value_type& pair : attrs) {
+    n->add(pair.first, pair.second);
+  }
 }
 
 void XMLReader::endElement(const std::string&) const {
+  --mDepth;
+  mNodeStack.pop_back();
 }
 
 void XMLReader::startElementHandler(void *userData, const XML_Char *name, const XML_Char **attrs) {
